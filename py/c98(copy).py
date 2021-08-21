693b00418ca3b2f2c84e274bafedf1d6e8e6e1a8
abhay kashyap <83275855+abhaykashyapkashyap@users.noreply.github.com>
2021-08-21 11:56:21 +0530

    Add files via upload

---
+++ py/c98(copy).py
@@ -0,0 +1,12 @@
+#fileoperation
+#f=open("text.txt")
+#filelines=f.readlines()
+#for line in filelines:
+#    print(line)
+#stringoperation
+#intro="my name ,is abhay,i am a boy"
+#words=intro.split(",")
+#print(words)
+def greet(name):
+    print("hello "+name+" how are you")
+greet("abhay")
